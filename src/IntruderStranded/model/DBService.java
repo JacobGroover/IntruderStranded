397306e6163da7420544040d12d87cc7a31cb0a7
fahmed10 <fahmed10@ggc.edu>
2024-11-02 10:51:25 -0400

    Create DBService class

---
+++ src/IntruderStranded/model/DBService.java
@@ -0,0 +1,42 @@
+package IntruderStranded.model;
+
+/**
+ * Class: DBService
+ * @author Fareed Ahmed
+ * @version 1.0
+ * Course: ITEC 3860 Fall 2024
+ * Written: November 2, 2024
+ * This class is a singleton which provides global access to a database object.
+ * This class cannot be instantiated or extended.
+ */
+public final class DBService {
+    private static DB db;
+
+    private DBService() {}
+
+    /**
+     * Method: start
+     * Starts the service with the given <code>DB</code>.
+     * @param db The <code>DB</code> to use.
+     */
+    public static void start(DB db) {
+        if (DBService.db != null) {
+            throw new IllegalStateException("DBService has already been started.");
+        }
+
+        if (db == null) {
+            throw new IllegalArgumentException("Argument db cannot be null.");
+        }
+
+        DBService.db = db;
+    }
+
+    /**
+     * Method: getDB
+     * Gets the current <code>DB</code>.
+     * @return The current <code>DB</code>.
+     */
+    static DB getDB() {
+        return db;
+    }
+}
