ce4ef9041f015f41ec6bca6a6ba3bae1d3f652f4
fahmed10 <fahmed10@ggc.edu>
2024-11-10 12:27:28 -0500

    Throw GameException instead of SQLException

--- src/IntruderStranded/model/InventoryDB.java
+++ src/IntruderStranded/model/InventoryDB.java
@@ -1,6 +1,7 @@
 package IntruderStranded.model;
 
 import IntruderStranded.controller.*;
+import IntruderStranded.gameExceptions.GameException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,61 +12,70 @@ import java.util.List;
 public interface InventoryDB {
 
 	/**
-	 * 
 	 * @param playerID
 	 */
-	default List<Item> getInventory(int playerID) throws SQLException {
-		ResultSet resultSet = DBService.getDB().queryPrepared("SELECT Item.*, Inventory.ItemQuantity FROM Inventory LEFT JOIN Item ON Inventory.ItemID = Item.ItemID WHERE PlayerID = ?", playerID);
+	default List<Item> getInventory(int playerID) throws GameException {
+		try {
+			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT Item.*, Inventory.ItemQuantity FROM Inventory LEFT JOIN Item ON Inventory.ItemID = Item.ItemID WHERE PlayerID = ?", playerID);
 
-		List<Item> items = new ArrayList<>();
-		while (resultSet.next()) {
-			Item item = new Item(resultSet.getInt("ItemID"));
-			item.setItemName(resultSet.getString("ItemName"));
-			item.setItemDescription(resultSet.getString("ItemDescription"));
+			List<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				Item item = new Item(resultSet.getInt("ItemID"));
+				item.setItemName(resultSet.getString("ItemName"));
+				item.setItemDescription(resultSet.getString("ItemDescription"));
 
-			items.addAll(Collections.nCopies(resultSet.getInt("ItemQuantity"), item));
+				items.addAll(Collections.nCopies(resultSet.getInt("ItemQuantity"), item));
+			}
+
+			resultSet.getStatement().close();
+			return items;
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
 		}
-
-		resultSet.getStatement().close();
-		return items;
 	}
 
 	/**
-	 * 
 	 * @param playerID
 	 * @param item
 	 */
-	default void addItem(int playerID, Item item) throws SQLException {
-		ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Inventory WHERE ItemID = ? AND PlayerID = ?", item.getItemID(), playerID);
-		boolean exists = resultSet.next();
+	default void addItem(int playerID, Item item) throws GameException {
+		try {
+			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Inventory WHERE ItemID = ? AND PlayerID = ?", item.getItemID(), playerID);
+			boolean exists = resultSet.next();
 
-		if (exists) {
+			if (exists) {
+				int quantity = resultSet.getInt("ItemQuantity");
+				DBService.getDB().updatePrepared("UPDATE Inventory SET Quantity = ?", quantity + 1);
+			} else {
+				DBService.getDB().updatePrepared("INSERT INTO Inventory (ItemID, PlayerID, ItemQuantity), (?, ?, ?)", item.getItemID(), playerID, 1);
+			}
+
+			resultSet.getStatement().close();
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
+		}
+	}
+
+	/**
+	 * @param playerID
+	 * @param item
+	 */
+	default void removeItem(int playerID, Item item) throws GameException {
+		try {
+			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Inventory WHERE ItemID = ? AND PlayerID = ?", item.getItemID(), playerID);
+			resultSet.next();
 			int quantity = resultSet.getInt("ItemQuantity");
-			DBService.getDB().updatePrepared("UPDATE Inventory SET Quantity = ?", quantity + 1);
-		} else {
-			DBService.getDB().updatePrepared("INSERT INTO Inventory (ItemID, PlayerID, ItemQuantity), (?, ?, ?)", item.getItemID(), playerID, 1);
+
+			if (quantity <= 1) {
+				DBService.getDB().updatePrepared("DELETE FROM Inventory WHERE ItemID = ?", item.getItemID());
+			} else {
+				DBService.getDB().updatePrepared("UPDATE Inventory SET ItemQuantity = ? WHERE ItemID = ?", quantity - 1, item.getItemID());
+			}
+
+			resultSet.getStatement().close();
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
 		}
-
-		resultSet.getStatement().close();
-	}
-
-	/**
-	 * 
-	 * @param playerID
-	 * @param item
-	 */
-	default void removeItem(int playerID, Item item) throws SQLException {
-		ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Inventory WHERE ItemID = ? AND PlayerID = ?", item.getItemID(), playerID);
-		resultSet.next();
-		int quantity = resultSet.getInt("ItemQuantity");
-
-		if (quantity <= 1) {
-			DBService.getDB().updatePrepared("DELETE FROM Inventory WHERE ItemID = ?", item.getItemID());
-		} else {
-			DBService.getDB().updatePrepared("UPDATE Inventory SET ItemQuantity = ? WHERE ItemID = ?", quantity - 1, item.getItemID());
-		}
-
-		resultSet.getStatement().close();
 	}
 
 }
--- src/IntruderStranded/model/PlayerDB.java
+++ src/IntruderStranded/model/PlayerDB.java
@@ -1,6 +1,7 @@
 package IntruderStranded.model;
 
 import IntruderStranded.controller.*;
+import IntruderStranded.gameExceptions.GameException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,57 +10,69 @@ import java.util.ArrayList;
 public class PlayerDB implements InventoryDB {
 
 	/**
-	 * 
 	 * @param player
 	 */
-	public void updatePlayer(Player player) throws SQLException {
-		DBService.getDB().updatePrepared("UPDATE Player SET Health = ?, PreviousRoom = ?, CurrentRoom = ?, Weapon = ?",
-				player.getHealth(), player.getPreviousRoom().getID(), player.getCurrentRoom().getID(), player.getWeapon());
+	public void updatePlayer(Player player) throws GameException {
+		try {
+			DBService.getDB().updatePrepared("UPDATE Player SET Health = ?, PreviousRoom = ?, CurrentRoom = ?, Weapon = ?",
+					player.getHealth(), player.getPreviousRoom().getID(), player.getCurrentRoom().getID(), player.getWeapon());
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
+		}
 	}
 
 	/**
-	 * 
 	 * @param playerID
 	 */
-	public Player getPlayer(int playerID) throws SQLException {
-		ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Player WHERE PlayerID = ?", playerID);
+	public Player getPlayer(int playerID) throws GameException {
+		try {
+			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Player WHERE PlayerID = ?", playerID);
 
-		Player player = new Player(playerID);
-		player.setUsername(resultSet.getString("Username"));
-		player.setScore(resultSet.getInt("Score"));
-		player.setWeapon(resultSet.getInt("Weapon"));
-		player.setHealth(resultSet.getInt("Health"));
+			Player player = new Player(playerID);
+			player.setUsername(resultSet.getString("Username"));
+			player.setScore(resultSet.getInt("Score"));
+			player.setWeapon(resultSet.getInt("Weapon"));
+			player.setHealth(resultSet.getInt("Health"));
 
-		RoomDB currentRoomDB = new RoomDB(resultSet.getInt("CurrentRoom"), playerID);
-		RoomDB previousRoomDB = new RoomDB(resultSet.getInt("PreviousRoom"), playerID);
-		player.setCurrentRoom(currentRoomDB.getRoom());
-		player.setPreviousRoom(previousRoomDB.getRoom());
+			RoomDB currentRoomDB = new RoomDB(resultSet.getInt("CurrentRoom"), playerID);
+			RoomDB previousRoomDB = new RoomDB(resultSet.getInt("PreviousRoom"), playerID);
+			player.setCurrentRoom(currentRoomDB.getRoom());
+			player.setPreviousRoom(previousRoomDB.getRoom());
 
-		resultSet.getStatement().close();
-		return player;
+			resultSet.getStatement().close();
+			return player;
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
+		}
 	}
 
 	/**
-	 * 
 	 * @param username
 	 * @param password
 	 * @param email
 	 */
-	public void addPlayer(String username, String password, String email) throws SQLException {
-		DBService.getDB().updatePrepared("INSERT INTO Player (Username, Password, Email, CurrentRoom, PreviousRoom, Weapon, Health) VALUES (?, ?, ?, ?, ?, ?, ?)",
-				username, password, email, 1, -1, -1, 100);
+	public void addPlayer(String username, String password, String email) throws GameException {
+		try {
+			DBService.getDB().updatePrepared("INSERT INTO Player (Username, Password, Email, CurrentRoom, PreviousRoom, Weapon, Health) VALUES (?, ?, ?, ?, ?, ?, ?)",
+					username, password, email, 1, -1, -1, 100);
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
+		}
 	}
 
 	/**
-	 * 
 	 * @param username
 	 * @param password
 	 */
-	public boolean checkLogin(String username, String password) throws SQLException {
-		ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Player WHERE Username = ? AND Password = ?", username, password);
-		boolean exists = resultSet.next();
-		resultSet.getStatement().close();
-		return exists;
+	public boolean checkLogin(String username, String password) throws GameException {
+		try {
+			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM Player WHERE Username = ? AND Password = ?", username, password);
+			boolean exists = resultSet.next();
+			resultSet.getStatement().close();
+			return exists;
+		} catch (SQLException exception) {
+			throw new GameException(exception.getMessage());
+		}
 	}
 
 }
