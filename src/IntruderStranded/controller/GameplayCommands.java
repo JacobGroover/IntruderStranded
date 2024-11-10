7bc7302d8d97ffe45dfa39662c31ffc60f7c8e1e
fahmed10 <fahmed10@ggc.edu>
2024-11-10 16:46:02 -0500

    Set up GameplayCommands

--- src/IntruderStranded/controller/GameplayCommands.java
+++ src/IntruderStranded/controller/GameplayCommands.java
@@ -8,7 +8,7 @@ import IntruderStranded.gameExceptions.*;
  * @version 1.0
  * Course: ITEC 3860 Fall 2024
  * Written: October 19th, 2024
- * 
+ *
  * This class – Is the Commands subclass for Gameplay Commands. Handles all user commands sent
  * from GameController and returns appropriate replies or exceptions.
  * Relevant while the player is in an active game session.
@@ -29,9 +29,8 @@ public class GameplayCommands extends Commands {
 	 * Calls parent one-argument constructor, then initializes booleans to false.
 	 * @param player
 	 */
-	GameplayCommands(Player player) {
-		// TODO - implement GameplayCommands.GameplayCommands
-		throw new UnsupportedOperationException();
+	public GameplayCommands(Player player) {
+		super(player);
 	}
 
 	/**
@@ -47,14 +46,33 @@ public class GameplayCommands extends Commands {
 	 * Inventory commands - INV, Store, Use <item>, Discard <item>, Close [calls the inventory method]
 	 * Movement Commands - TEL, North, South, East, West, Flee [calls the move method]
 	 * Battle commands - Attack, Defend
-	 * 
+	 *
 	 * Throws an exception for an invalid command
 	 * @param command
 	 */
-	@Override()
+	@Override
 	String executeCommand(String command) throws GameException {
-		// TODO - implement GameplayCommands.executeCommand
-		throw new UnsupportedOperationException();
+		return switch (command) {
+			case "HELP" -> help();
+			case "HINT" -> hint();
+			case "LOOK" -> look();
+			case "SAVE" -> saveGame();
+			case "INV" -> inventory(command);
+			case "MOVE" -> move(command);
+			case "EXIT" -> exit(command);
+			case "ATTACK" -> attack();
+			case "DEFEND" -> defend();
+			case "FLEE" -> flee();
+			case "USE" -> useItem();
+			case "TEL" -> teleport(command);
+			default -> {
+				if (Direction.parseDirection(command) != null) {
+					yield move(command);
+				}
+
+				throw new GameException("Invalid command");
+			}
+		};
 	}
 
 	/**
@@ -91,19 +109,19 @@ public class GameplayCommands extends Commands {
 	 * Opens the player's inventory and allows them to interact with their inventory.
 	 * If command String parameter equals "inv" then sets isManagingInventory boolean to true and
 	 * return a String listing the contents of the inventory by calling player.displayInventory method.
-	 * 
+	 *
 	 * If isManagingInventory boolean is already true:
-	 * 
+	 *
 	 * If the command parameter equals "Use <item>" where <item> is an item in the player's inventory,
 	 * then uses the item by calling player.useItem method. Otherwise returns a String indicating
 	 * the item is not in the player's inventory.
-	 * 
+	 *
 	 * If the command parameter equals "Discard <item>" where <item> is an item in the player's
 	 * inventory, then removes the item from inventory and adds it to the room by calling
 	 * player.removeItem method and player.getCurrentRoom.addItem method. Otherwise, returns
 	 * a String indicating the item is not in the player's inventory (this is checked using the
 	 * player.getInventory method).
-	 * 
+	 *
 	 * If the command parameter equals "Help" then return a String listing the available commands (Use <item>, Discard <item>, Close, Help).
 	 * If the command parameter equals "Close" then close the inventory menu by setting the isManagingInventory
 	 * boolean to false.
@@ -138,7 +156,7 @@ public class GameplayCommands extends Commands {
 	 * Help
 	 * Save Game
 	 * Load Game
-	 * INV (if in inventory, then only Store, Use <item>, Discard <item>, Close, and Exit commands are available)
+	 * INV (if in inventory, then only Store, Use &lt;item&gt;, Discard &lt;item&gt;, Close, and Exit commands are available)
 	 * TEL (not available during room event, i.e. monster encounter or puzzle)
 	 * North (not available during room event, i.e. monster encounter or puzzle)
 	 * South (not available during room event, i.e. monster encounter or puzzle)
@@ -146,7 +164,7 @@ public class GameplayCommands extends Commands {
 	 * West (not available during room event, i.e. monster encounter or puzzle)
 	 * Flee (only available during room event, i.e. monster encounter or puzzle)
 	 */
-	@Override()
+	@Override
 	String help() {
 		// TODO - implement GameplayCommands.help
 		throw new UnsupportedOperationException();
@@ -156,18 +174,18 @@ public class GameplayCommands extends Commands {
 	 * Method: exit
 	 * If isExiting boolean is false, sets isExiting boolean to true, then prompts the player by returning
 	 * "Do you want to save your game?"
-	 * 
+	 *
 	 * If the isExiting boolean is already true, then reads the String command parameter.
 	 * If the command parameter equals "yes" or "y" then sets the isExiting boolean to false, saves the game
 	 * in the database and calls the changeGameState method, changing the game state to MainMenuCommands.
-	 * 
+	 *
 	 * If the command parameter equals "no" or "n" then sets the isExiting boolean to false, does NOT
 	 * save the game, and calls the changeGameState method, changing the game state to MainMenuCommands.
 	 * If the commands parameter is any other String, sets the isExiting boolean to false and throws a
 	 * GameException for unrecognized command.
 	 * @param command
 	 */
-	@Override()
+	@Override
 	String exit(String command) throws GameException {
 		// TODO - implement GameplayCommands.exit
 		throw new UnsupportedOperationException();
@@ -198,7 +216,7 @@ public class GameplayCommands extends Commands {
 	 * Returns a String to the calling method containing:
 	 * Welcome message and Game title
 	 * list of available commands
-	 * 
+	 *
 	 * Room details for first room
 	 */
 	protected String getIntroText() {
@@ -211,30 +229,30 @@ public class GameplayCommands extends Commands {
 	 * If teleportCounter = -1:
 	 * Calls player.getCurrentRoom.isTeleport() method. If that method returns true, increment
 	 * teleportCounter to 0. Otherwise, throws a GameException to calling method for unrecognized command.
-	 * 
+	 *
 	 * If teleportCounter = 0:
 	 * Increments teleportCounter.
 	 * Returns "Where would you like to teleport? Level -2(Cell), Level -1(Armory), Level 0(Inside),
 	 * Level 0(Outside). Please enter a number."
-	 * 
+	 *
 	 * If teleportCounter = 1:
 	 * If input parameter equals "0" or "-2" or "-1" Set teleportLevel class attribute to input parameter.
 	 * If input parameter equals "0", increments teleportCounter and returns "Please enter
 	 * "inside" or "outside" "
 	 * If input parameter equals "-2" or "-1", increment teleportCounter (increments without returning anything).
 	 * If anything other than -2, -1, or 0 is entered, throw a GameException with "This level does not exist."
-	 * 
+	 *
 	 * If teleportCounter = 2:
 	 * If teleportLevel = "0" and input parameter is not "inside" or "outside", then return "Please enter "inside" or "outside" "
 	 * If teleportLevel = "0" and input parameter equals "inside" or "outside", or teleportLevel
 	 * = "-2" or "-1" then increment teleportCounter and return "Are you sure you want to teleport?"
-	 * 
+	 *
 	 * If teleportCounter = 3:
 	 * If input parameter = "yes", call player.getCurrentRoom.leaveRoom() method and then
 	 * assign previous room to current room, then assign the return of that method to current room.
-	 * 
+	 *
 	 * If input parameter = "no", then set teleportCounter to -1 and teleportLevel to null
-	 * 
+	 *
 	 * Otherwise return "Please enter yes or no."
 	 * @param command
 	 */
