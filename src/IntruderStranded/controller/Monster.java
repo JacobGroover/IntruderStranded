c13d249df637d621e024b5b98b1932618b643297
fahmed10 <fahmed10@ggc.edu>
2024-11-11 09:16:52 -0500

    Update Monster shell

--- src/IntruderStranded/controller/Monster.java
+++ src/IntruderStranded/controller/Monster.java
@@ -1,18 +1,36 @@
 package IntruderStranded.controller;
 
+import IntruderStranded.model.RoomDB;
+
 import java.util.*;
 
 public class Monster extends Entity implements RoomEvent {
-
 	private String name;
-	private List<Item> rewards;
+	private int damage;
+	private final RoomDB roomDB;
 
-	public Monster(int id) {
+	public Monster(int id, int roomID, int playerID) {
 		super(id);
+		this.roomDB = new RoomDB(roomID, playerID);
 	}
 
 	public List<Item> getRewards() {
-		return this.rewards;
+		throw new UnsupportedOperationException();
 	}
 
+    public int getDamage() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setDamage(int damage) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setName(String name) {
+        throw new UnsupportedOperationException();
+    }
 }
