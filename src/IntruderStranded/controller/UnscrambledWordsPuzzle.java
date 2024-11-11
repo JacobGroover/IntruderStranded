850eedb9724680d94f551e8713eef3f4100d978e
Hannah Jensen <hhart@ggc.edu>
2024-11-11 18:12:26 -0500

    filled out shells and updated constructer

--- src/IntruderStranded/controller/UnscrambledWordsPuzzle.java
+++ src/IntruderStranded/controller/UnscrambledWordsPuzzle.java
@@ -1,44 +1,72 @@
 package IntruderStranded.controller;
 
-import java.util.List;
+import IntruderStranded.gameExceptions.GameException;
+import IntruderStranded.model.RewardDB;
+import IntruderStranded.model.RoomDB;
+
+import java.util.*;
 
 /**
  * Class: UnscrambleWordsPuzzle
- * @author
+ * @author Hannah Jensen
  * @version 1.0
  * Course: ITEC 3860 Fall 2024
- * Written: October 25th, 2024
+ * Written: November 11th, 2024
  * This class details the implementation for the UnscrambleWordsPuzzle
  */
 public class UnscrambledWordsPuzzle extends Puzzle {
 
 	private String answerWord;
 	private String scrambledWord;
+	private final RoomDB roomDB;
+
 
 	/**
 	 * No-argument Constructor for UnscrambledWordsPuzzle class
 	 * Calls setupPuzzle() method to assign the answerWord, scrambledWord, and puzzleCounter class attributes.
 	 */
-	public UnscrambledWordsPuzzle(int id) {
+	public UnscrambledWordsPuzzle(int id, int roomID, int playerID) {
 		super(id);
-		// TODO - implement UnscrambledWordsPuzzle.UnscrambledWordsPuzzle
-		throw new UnsupportedOperationException();
+		roomDB = new RoomDB(roomID, playerID);
+		setupPuzzle();
 	}
 
 	/**
-	 * Method: run
-	 * If puzzleCounter is -1, returns "Unscramble the planet's name: " + scrambledWord and increment puzzleCounter.
-	 * If puzzleCounter is not -1, check if the input parameter is equal to answerWord.
-	 * If it is not equal, increment the puzzleCounter and return "Incorrect word, try again."
-	 * If the puzzleCounter reaches 3, set puzzleCounter to -1 and return "You lost the puzzle."
-	 * If it is equal, return "You have successfully solved the puzzle!" and call getRewards method from
-	 * the implemented RoomEvent interface.
-	 * @param cmd
-	 */
+     * Method: run
+     * If puzzleCounter is -1, returns "Unscramble the planet's name: " + scrambledWord and increment puzzleCounter.
+     * If puzzleCounter is not -1, check if the input parameter is equal to answerWord.
+     * If it is not equal, increment the puzzleCounter and return "Incorrect word, try again."
+     * If the puzzleCounter reaches 3, set puzzleCounter to -1 and return "You lost the puzzle."
+     * If it is equal, return "You have successfully solved the puzzle!" and call getRewards method from
+     * the implemented RoomEvent interface.
+     *
+     */
 	@Override()
-	String run(String cmd) {
-		// TODO - implement UnscrambledWordsPuzzle.run
-		throw new UnsupportedOperationException();
+    void run(String cmd) {
+		Scanner input = new Scanner(System.in);
+
+		if (puzzleCounter == -1) {
+			System.out.println("Unscramble the planets name: " + scrambledWord);
+			puzzleCounter ++;
+		}
+
+		while(puzzleCounter <= 3) {
+			String answer = input.nextLine();
+			if (answer.equalsIgnoreCase(answerWord)) {
+				System.out.println("You have successfully solved the puzzle!");
+				getRewards();
+				break;
+			}else if (puzzleCounter == 3) {
+				System.out.println("You have lost the puzzle.");
+				puzzleCounter ++;
+			}
+			else {
+				System.out.println("Incorrect word, try again.");
+				puzzleCounter ++;
+			}
+		}
+
+		setupPuzzle();
 	}
 
 	/**
@@ -59,9 +87,40 @@ public class UnscrambledWordsPuzzle extends Puzzle {
 	 * Sets the puzzleCounter class attribute to -1.
 	 */
 	void setupPuzzle() {
-		// TODO - implement UnscrambledWordsPuzzle.setupPuzzle
-		throw new UnsupportedOperationException();
+		Random random = new Random();
+
+		List<String> planets = new ArrayList<>();
+		planets.add("MERCURY");
+		planets.add("VENUS");
+		planets.add("EARTH");
+		planets.add("MARS");
+		planets.add("JUPITER");
+		planets.add("SATURN");
+		planets.add("URANUS");
+		planets.add("NEPTUNE");
+
+		answerWord = planets.get(random.nextInt(planets.size()));
+
+		scrambledWord = scrambleWord(answerWord);
+		
+		puzzleCounter = -1;
 	}
+
+	public String scrambleWord(String word) {
+		List<Character> charWord = new ArrayList<>();
+		for(char c : word.toCharArray()) {
+			charWord.add(c);
+		}
+
+		Collections.shuffle(charWord);
+
+		StringBuilder scrambledWord = new StringBuilder();
+		for(char c : charWord) {
+			scrambledWord.append(c);
+		}
+		return scrambledWord.toString();
+	}
+
 
 	@Override
 	String getHint() {
@@ -74,8 +133,11 @@ public class UnscrambledWordsPuzzle extends Puzzle {
 	 */
 	@Override()
 	public List<Item> getRewards() {
-		// TODO - implement UnscrambledWordsPuzzle.getRewards
-		throw new UnsupportedOperationException();
-	}
+        try {
+            return RewardDB.getRewards();
+        } catch (GameException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
