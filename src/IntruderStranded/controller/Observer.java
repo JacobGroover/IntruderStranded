30100ecb03b9710b72d73e5fac24c4d2d9b62bf5
Jacob Groover <jgroover@ggc.edu>
2024-11-10 22:00:33 -0500

    Implement Observer.java interface and update its class and method comments

--- src/IntruderStranded/controller/Observer.java
+++ src/IntruderStranded/controller/Observer.java
@@ -1,9 +1,21 @@
 package IntruderStranded.controller;
 
+/**
+ * Class: GameController
+ * @author Jacob Groover
+ * @version 1.0
+ * Course: ITEC 3860 Fall 2024
+ * Written: November 6th, 2024
+ *
+ * This interface – Utilizes the observer design pattern by providing an observer interface for updating
+ * subscribers based on the state of observed objects. Specifically used to update GameController with the
+ * current game state of the active Commands class.
+ */
 public interface Observer<T> {
 
 	/**
-	 * 
+	 * Method: onUpdate
+	 * Updates subscribers to this Observer interface. Implementation varies between subscribers.
 	 * @param arg
 	 */
 	abstract void onUpdate(T arg);
