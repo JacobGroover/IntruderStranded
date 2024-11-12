b47dbf4b81071b92e7cf2c2684d4e9b477940686
fahmed10 <fahmed10@ggc.edu>
2024-11-12 18:15:02 -0500

    Create SaveManager

---
+++ src/IntruderStranded/model/SaveManager.java
@@ -0,0 +1,31 @@
+package IntruderStranded.model;
+
+import IntruderStranded.gameExceptions.GameException;
+
+import java.sql.SQLException;
+
+/**
+ * Class: SaveManager
+ * @author Fareed Ahmed
+ * @version 1.0
+ * Course: ITEC 3860 Fall 2024
+ * Written: November 12, 2024
+ *
+ * This class gives the Controller package access to the DB.commitTransaction() method,
+ * so that it can save the game.
+ */
+public class SaveManager {
+    /**
+     * Method: saveGame
+     * Saves the game, making permanent any changes which have occurred since the last
+     * save.
+     * @throws GameException
+     */
+    public static void saveGame() throws GameException {
+        try {
+            DBService.getDB().commitTransaction();
+        } catch (SQLException exception) {
+            throw new GameException(exception.getMessage());
+        }
+    }
+}
