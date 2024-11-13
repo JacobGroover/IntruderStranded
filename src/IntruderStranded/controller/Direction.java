548c329f82c136381faff02df057ca587bce405c
fahmed10 <fahmed10@ggc.edu>
2024-11-13 13:44:30 -0500

    Create Direction.getTitleCaseString

--- src/IntruderStranded/controller/Direction.java
+++ src/IntruderStranded/controller/Direction.java
@@ -36,4 +36,8 @@ public enum Direction {
 	public boolean isTeleport() {
 		return !physicalDirectionMap.containsValue(this);
 	}
+
+	public String getTitleCaseString() {
+		return name().charAt(0) + name().toLowerCase().substring(1);
+	}
 }
