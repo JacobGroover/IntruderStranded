b2e90c235b36d5ee3f0fb18a10a33d147b6d4cac
fahmed10 <fahmed10@ggc.edu>
2024-11-14 18:49:59 -0500

    Comment model

--- src/IntruderStranded/model/VisitRoomDB.java
+++ src/IntruderStranded/model/VisitRoomDB.java
@@ -5,7 +5,21 @@ import IntruderStranded.gameExceptions.GameException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Class: VisitRoomDB
+ * @author Fareed Ahmed
+ * @version 1.0
+ * Course: ITEC 3860 Fall 2024
+ * Written: November 14, 2024
+ *
+ * This class handles getting and setting the room visited data from the database.
+ */
 public record VisitRoomDB(int roomID, int playerID) {
+	/**
+	 * Method: getVisited
+	 * Gets the visited status of this room.
+	 * @return True if this room is marked as visited, otherwise false.
+	 */
 	boolean getVisited() throws GameException {
 		try {
 			ResultSet resultSet = DBService.getDB().queryPrepared("SELECT * FROM VisitRoom WHERE PlayerID = ? AND RoomID = ?", playerID(), roomID());
@@ -17,6 +31,10 @@ public record VisitRoomDB(int roomID, int playerID) {
 		}
 	}
 
+	/**
+	 * Method: setVisited
+	 * Sets this room as visited.
+	 */
 	void setVisited() throws GameException {
 		try {
 			DBService.getDB().updatePrepared("INSERT OR REPLACE INTO VisitRoom (PlayerID, RoomID) VALUES (?, ?)", playerID(), roomID());
