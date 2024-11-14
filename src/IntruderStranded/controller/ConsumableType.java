c9bc24142c9929fc03798e6bc0e30a0c5706ab7f
fahmed10 <fahmed10@ggc.edu>
2024-11-14 18:51:11 -0500

    Comment ConsumableType enum

--- src/IntruderStranded/controller/ConsumableType.java
+++ src/IntruderStranded/controller/ConsumableType.java
@@ -1,5 +1,15 @@
 package IntruderStranded.controller;
 
+/**
+ * Enum: ConsumableType
+ * @author Fareed Ahmed
+ * @version 1.0
+ * Course: ITEC 3860 Fall 2024
+ * Written: November 14, 2024
+ *
+ * This enum identifies consumable items by their consumable type, so the correct
+ * logic can be run when the item is used.
+ */
 public enum ConsumableType {
     NONE,
     MED_PACK,
